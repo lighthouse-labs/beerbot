1a94cf093517174bb07f5cb234f6caa4d5954d98
Monica Olinescu <monica.olinescu@gmail.com>
2015-02-17 10:26:13 -0800

    Make bot leave all channels

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var autoMark = true;
 
 var slack = new Slack(token, autoReconnect, autoMark);
 
+slack.on('connect', function(){
+  // leave all channels you are connected to
+  var key;
+  for (key in slack.channels) {
+    if (slack.channels[key].is_member) {
+      slack.channels[key].leave();
+    }
+  }
+});
+
 slack.on('message', function(message){
   var channel = slack.getChannelGroupOrDMByID(message.channel);
   var user = slack.getUserByID(message.user);
@@ -13,6 +23,7 @@ slack.on('message', function(message){
 
   // respond to the sender with the same message
   channel.send(text);
+
   console.log("!!!! Responded to %s.", user.name);
 });
 
