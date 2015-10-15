2e5536d36114e5b5bb5bd43f416f17c94e4a79ac
Monica Olinescu <monica.olinescu@gmail.com>
2015-10-15 11:08:38 -0700

    Connect to Slack

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ slack = new SlackClient(process.env.SLACK_TOKEN, true, true)
 
 slack.on('open', function() {
   console.log("Connected to Slack WS Server");
+
+  // leave general so... no spam
+  leaveAllChannels();
+  sayHelloToDon();
+
+  // private functions below
+  // var leaveAllChannels = function(){
+  function leaveAllChannels() {
+    for (var id in slack.channels) {
+      var channel = slack.channels[id];
+      if (channel.is_member) {
+        console.log("Leaving channel " + channel.name);
+        channel.leave();
+      }
+    }
+  }
+
+  function sayHelloToDon() {
+    // we won't spam don now. just an example
+  }
 });
 
 slack.on('error', function(error) {
-  console.error('Error: ' + error);
+  console.error('Error in Slack: ', error);
+});
 });
 
 slack.login(); // start connection to Slack websockets server
