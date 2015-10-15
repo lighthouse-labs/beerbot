c7ce8db722db28c0a1c77d0835e4bb324e8b3b54
Monica Olinescu <monica.olinescu@gmail.com>
2015-10-15 10:02:58 -0700

    Beerbot connects to Slack

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+SlackClient = require('slack-client');
 
+console.log("Starting beerbot server");
 
+slack = new SlackClient(process.env.SLACK_TOKEN, true, true)
 
+slack.on('open', function() {
+  console.log("Connected to Slack WS Server");
 });
 
 slack.on('error', function(error) {
+  console.error('Error: ' + error);
 });
 
+slack.login(); // start connection to Slack websockets server
